09dd71736e862043f633abc81443c4c1312e1175
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-02 14:02:23 -0300

    create OrderItem component

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,18 +1,19 @@
 import { FlatList, Platform, Text } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { useSelector } from 'react-redux'
+import { OrderItem } from '../../components/shop/OrderItem'
 import { CustomHeaderButton } from '../../components/UI/HeaderButton'
 
-export function OrdersScreen(props){
+export function OrdersScreen(){
   const orders = useSelector(state => state.orders.orders)
 
   return (
     <FlatList
       data={orders}
       keyExtractor={item => item.id}
-      renderItem={itemData => {
-        <Text>{itemData.item.totalAmount}</Text>
-      }}
+      renderItem={itemData => (
+        <OrderItem amount={itemData.item.totalAmount} date={itemData.item.readableDate} />
+      )}
     />
   )
 }
