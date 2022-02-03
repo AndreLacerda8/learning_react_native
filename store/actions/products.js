c23bcdfc6620370221f5e60f4f139604ec88a654
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-03 10:53:37 -0300

    store products on a server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,3 +1,5 @@
+import { URL_FIREBASE } from '@env'
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
@@ -10,14 +12,32 @@ export function deleteProduct(productId){
 }
 
 export function createProduct(title, description, imageUrl, price){
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    const response = await fetch(`${URL_FIREBASE}/products.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    })
+
+    const responseData = await response.json()
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: responseData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    })
   }
 }
 
