667075875145b8657cc6f76a6b41a3d77834641a
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-03 13:49:08 -0300

    add order in database

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,31 @@
+import { URL_FIREBASE } from '@env'
+
 export const ADD_ORDER = 'ADD_ORDER'
 
 export function addOrder(cartItems, totalAmount){
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  return async dispatch => {
+    const date = new Date()
+    const response = await fetch(`${URL_FIREBASE}/orders/u1.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date: date.toISOString()
+      })
+    })
+
+    if(!response.ok){
+      throw new Error('Something went wrong!')
+    }
+
+    const responseData = await response.json()
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: { id: responseData.name, items: cartItems, amount: totalAmount, date }
+    })
   }
 }
