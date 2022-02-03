dc058ffc4ad9b9fe6f3511a5d820b0722a7d3353
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-03 14:16:14 -0300

    add pull to refresh

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -11,19 +11,20 @@ import * as productsActions from '../../store/actions/products'
 
 export function ProductsOverviewScreen(props){
   const [isLoading, setIsLoading] = useState(false)
+  const [isRefreshing, setIsRefreshing] = useState(false)
   const [error, setError] = useState()
   const products = useSelector(state => state.products.availableProducts)
   const dispatch = useDispatch()
 
   const loadProducts = useCallback(async () => {
     setError(null)
-    setIsLoading(true)
+    setIsRefreshing(true)
     try{
       await dispatch(productsActions.fetchProducts())
     } catch(err){
       setError(err.message)
     }
-    setIsLoading(false)
+    setIsRefreshing(false)
   }, [dispatch, setIsLoading, setError])
 
   useEffect(() => {
@@ -35,7 +36,10 @@ export function ProductsOverviewScreen(props){
   }, [loadProducts])
 
   useEffect(() => {
-    loadProducts()
+    setIsLoading(true)
+    loadProducts().then(() => {
+      setIsLoading(false)
+    })
   }, [dispatch, loadProducts])
 
   function selectItemHandler(id, title){
@@ -72,6 +76,8 @@ export function ProductsOverviewScreen(props){
 
   return (
     <FlatList
+      onRefresh={loadProducts}
+      refreshing={isRefreshing}
       data={products}
       keyExtractor={item => item.id}
       renderItem={itemData => (
