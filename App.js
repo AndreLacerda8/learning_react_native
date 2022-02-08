86a2c4c179ec7bff3ca3d45d48fe9795f225227a
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-08 09:56:28 -0300

    work with notifications in foreground and background

--- App.js
+++ App.js
@@ -4,6 +4,15 @@ import { Button, StyleSheet, Text, View } from 'react-native'
 import * as Notifications from 'expo-notifications'
 import * as Permissions from 'expo-permissions'
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true
+    }
+  }
+})
+
 export default function App() {
   useEffect(() => {
     Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
@@ -18,11 +27,26 @@ export default function App() {
     })
   }, [])
 
+  useEffect(() => {
+    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log('RESPONSE: ', response)
+    })
+
+    const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+    return () => {
+      backgroundSubscription.remove()
+      foregroundSubscription.remove()
+    }
+  }, [])
+
   function triggerNotificationHandler(){
     Notifications.scheduleNotificationAsync({
       content: {
         title: 'My first local notification',
-        body: 'This is the first notification we are sending'
+        body: 'This is the first notification we are sending',
+        data: { myData: 'Some data' }
       },
       trigger: {
         seconds: 5
