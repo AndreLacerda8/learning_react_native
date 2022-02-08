fab13d2127342737e708877ae790220b73da9fe6
Andre Lacerda <andrlacerda180@gmail.com>
2022-02-08 09:33:25 -0300

    send first notification

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
 
 export default function App() {
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
+      if(statusObj.status !== 'granted'){
+        return Permissions.askAsync(Permissions.NOTIFICATIONS)
+      }
+      return statusObj
+    }).then(statusObj => {
+      if(statusObj.status !== 'granted'){
+        return
+      }
+    })
+  }, [])
+
+  function triggerNotificationHandler(){
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the first notification we are sending'
+      },
+      trigger: {
+        seconds: 5
+      }
+    })
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>Hello World</Text>
+      <Button title='Trigger Notification' onPress={triggerNotificationHandler} />
+      <StatusBar style='auto' />
     </View>
   );
 }
